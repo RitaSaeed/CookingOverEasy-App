64fb3dd9388db0e9f0ad82ade7b3722281e10130
sylloyd <sylloyd@radford.edu>
2023-03-24 12:31:01 -0400

    .

--- app/src/main/java/com/example/cookingovereasy/Cookbook.java
+++ app/src/main/java/com/example/cookingovereasy/Cookbook.java
@@ -21,7 +21,7 @@ public class Cookbook extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cookbook);
 
         auth = FirebaseAuth.getInstance();
         button = findViewById(R.id.logout);
