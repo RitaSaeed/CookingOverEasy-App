32b194e72055d6afb5b7fcb4268e092ab87213f2
Tyler Chitwood <tdchitwood@radford.edu>
2023-03-26 23:54:31 -0400

    updated search fragment
    updated the search fragment to display a list of scrollable random numbers

--- app/src/main/java/com/example/cookingovereasy/SearchFragment.java
+++ app/src/main/java/com/example/cookingovereasy/SearchFragment.java
@@ -3,6 +3,8 @@ package com.example.cookingovereasy;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +12,20 @@ import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
 
+    private RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(new RandomNumListAdapter(1234));
+
+        return view;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        // return inflater.inflate(R.layout.fragment_search, container, false);
     }
 }
