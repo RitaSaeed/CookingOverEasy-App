f017ef2599254e9d8259ff14af037e3558b920ce
sylloyd <sylloyd@radford.edu>
2023-03-28 13:43:42 -0400

    Grocery List
    Grocery List

--- app/src/main/java/com/example/cookingovereasy/GroceryListFragment.java
+++ app/src/main/java/com/example/cookingovereasy/GroceryListFragment.java
@@ -2,17 +2,58 @@ package com.example.cookingovereasy;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
 public class GroceryListFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private ArrayList<Ingredient> ingredientArrayList;
+    private String[] ingredientName;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_grocery_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dataInitialize();
+
+        recyclerView = view.findViewById(R.id.recycler_grocery_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+        IngredientAdapter adapter = new IngredientAdapter(getContext(), ingredientArrayList);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void dataInitialize() {
+
+        ingredientArrayList = new ArrayList<>();
+        ingredientName = new String[] {
+                "Eggs", "Milk", "Chicken", "Steak", "Carrots", "Apples", "Broccoli", "Mushrooms",
+                "Olive Oil", "Sugar", "Flour", "Paprika", "Italian Seasoning", "Bread Crumbs",
+                "Salt", "Pasta", "Alfredo Sauce", "Orange", "Mango", "Avocado", "Banana", "Asaparagus"
+        };
+
+
+        for (int i = 0; i < ingredientName.length; i++) {
+            Ingredient ingredient = new Ingredient(ingredientName[i]);
+            ingredientArrayList.add(ingredient);
+            System.out.println(ingredient.name);
+        }
     }
 }
---
+++ app/src/main/java/com/example/cookingovereasy/Ingredient.java
@@ -0,0 +1,11 @@
+package com.example.cookingovereasy;
+
+public class Ingredient {
+    String name;
+
+
+    public Ingredient(String name) {
+        this.name = name;
+
+    }
+}
