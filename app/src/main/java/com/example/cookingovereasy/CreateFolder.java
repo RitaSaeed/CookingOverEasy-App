44d89a33b48e4be3f8c4ba8dd50ac6bd182aca95
RitaSaeed <rita.elizabeth15@yahoo.com>
2023-03-31 12:08:15 -0400

    add category
    made a new activity for making new categories

---
+++ app/src/main/java/com/example/cookingovereasy/CreateFolder.java
@@ -0,0 +1,39 @@
+package com.example.cookingovereasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class CreateFolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_folder);
+
+        CookBookFragment existingFragment = new CookBookFragment();
+
+       // FrameLayout container = findViewById(R.id.container_view);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Begin a FragmentTransaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Create an instance of the existing fragment
+
+
+        // Add the existing fragment to the container view
+       fragmentTransaction.add(R.id.container_view, existingFragment);
+
+        // Commit the transaction
+        fragmentTransaction.commit();
+
+
+
+
+    }
+}
+
