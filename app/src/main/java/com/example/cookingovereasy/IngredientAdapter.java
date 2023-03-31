238f7c785a2fb9c5721f0b3d5bddda6b91fa5102
Tyler Chitwood <tdchitwood@radford.edu>
2023-03-30 22:46:53 -0400

    updated checkboxes
    updated the state of the checkboxes on the ingredients fragment to be persistent when scrolling

--- app/src/main/java/com/example/cookingovereasy/IngredientAdapter.java
+++ app/src/main/java/com/example/cookingovereasy/IngredientAdapter.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -34,9 +36,26 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.My
 
     @Override
     public void onBindViewHolder(@NonNull IngredientAdapter.MyViewHolder holder, int position) {
-
         Ingredient ingredient = ingredientArrayList.get(position);
+        //holder.checkBox.setText("Checkbox " + position);
+        holder.checkBox.setChecked(ingredientArrayList.get(position).getSelected());
         holder.ingredientName.setText(ingredient.name);
+
+        holder.checkBox.setTag(position);
+        holder.checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer pos = (Integer) holder.checkBox.getTag();
+                //Toast.makeText(context, ingredientArrayList.get(pos).getName() + " clicked!",
+                //       Toast.LENGTH_SHORT).show();
+
+                if (ingredientArrayList.get(pos).getSelected()) {
+                    ingredientArrayList.get(pos).setSelected(false);
+                } else {
+                    ingredientArrayList.get(pos).setSelected(true);
+                }
+            }
+        });
 
     }
 
@@ -48,9 +67,11 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.My
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView ingredientName;
+        protected CheckBox checkBox;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            checkBox = (CheckBox) itemView.findViewById(R.id.checkBoxIngredient);
             ingredientName = itemView.findViewById(R.id.ingredientName);
         }
     }
