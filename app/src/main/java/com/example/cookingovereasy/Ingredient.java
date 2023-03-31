30cfeffda81eb2ebbda6b22b1b86b64f093ebcf2
Tyler Chitwood <tdchitwood@radford.edu>
2023-03-30 22:27:22 -0400

    updated ingredient.java
    added accessors and modifiers for ingredient object

--- app/src/main/java/com/example/cookingovereasy/Ingredient.java
+++ app/src/main/java/com/example/cookingovereasy/Ingredient.java
@@ -2,10 +2,26 @@ package com.example.cookingovereasy;
 
 public class Ingredient {
     String name;
+    boolean isSelected;
 
 
     public Ingredient(String name) {
         this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
     }
 }
