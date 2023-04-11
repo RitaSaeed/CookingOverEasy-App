672fcb28c0837238e105d4b9c5428c42eaf781c3
Tyler Chitwood <tdchitwood@radford.edu>
2023-04-10 21:05:41 -0400

    created model classes
    created the model classes necessary to represent each component of a recipe, as well as the recipe itself

---
+++ app/src/main/java/com/example/cookingovereasy/Models/Measures.java
@@ -0,0 +1,6 @@
+package com.example.cookingovereasy.Models;
+
+public class Measures {
+    public Us us;
+    public Metric metric;
+}
---
+++ app/src/main/java/com/example/cookingovereasy/Models/Temperature.java
@@ -0,0 +1,6 @@
+package com.example.cookingovereasy.Models;
+
+public class Temperature {
+    public double number;
+    public String unit;
+}
