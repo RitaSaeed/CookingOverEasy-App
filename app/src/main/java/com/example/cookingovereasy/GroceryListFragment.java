1988068110028f47697cc4e760ae2334216f662c
Tyler Chitwood <tdchitwood@radford.edu>
2023-04-14 02:25:05 -0400

    ingredient list
    WIP

--- app/src/main/java/com/example/cookingovereasy/GroceryListFragment.java
+++ app/src/main/java/com/example/cookingovereasy/GroceryListFragment.java
@@ -1,9 +1,16 @@
 package com.example.cookingovereasy;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -25,6 +32,7 @@ public class GroceryListFragment extends Fragment implements PopupMenu.OnMenuIte
 
     private RecyclerView recyclerView;
     private ArrayList<Ingredient> ingredientArrayList;
+    private ArrayList<String> ingredients;
 
     ImageView remove;
     private String[] ingredientName;
@@ -52,7 +60,8 @@ public class GroceryListFragment extends Fragment implements PopupMenu.OnMenuIte
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        ingredientArrayList = new ArrayList<>();
+//        ingredientArrayList.add(new Ingredient(getArguments().getString("newIngredient")));
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_grocery_list, container, false);
     }
@@ -67,15 +76,27 @@ public class GroceryListFragment extends Fragment implements PopupMenu.OnMenuIte
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+//        ingredientArrayList.add(new Ingredient(getArguments().getString("newIngredient")));
         add = view.findViewById(R.id.addIcon);
 
+        ActivityResultLauncher<Intent> startForIngredient = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result != null && result.getResultCode() == -1) {
+                    if (result.getData() != null) {
+                        ingredientArrayList.add(new Ingredient(getArguments().getString("newIngredient")));
+                        Toast.makeText(context, "current ingredient: " + getArguments().getString("newIngredient"), Toast.LENGTH_SHORT).show();
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(getActivity(), AddToGroceryList.class);
+//                startForIngredient.launch(intent);
                 startActivity(intent);
             }
         });
@@ -86,7 +107,7 @@ public class GroceryListFragment extends Fragment implements PopupMenu.OnMenuIte
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
 
-        dataInitialize();
+        //dataInitialize();
 
         remove = view.findViewById(R.id.imageViewRemove);
         remove.setOnClickListener(new View.OnClickListener() {
