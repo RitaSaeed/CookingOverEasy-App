ee194a650eea90585b6b75988205d61912ebd92f
Tyler Chitwood <tdchitwood@radford.edu>
2023-04-30 22:55:48 -0400

    updated cookbook button

--- app/src/main/java/com/example/cookingovereasy/CookBookFragment.java
+++ app/src/main/java/com/example/cookingovereasy/CookBookFragment.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -42,7 +43,7 @@ import java.util.Map;
  */
 public class CookBookFragment extends Fragment implements CategoryAdapter.EventListener{
 
-    ImageView myCookBook; //declaring buttons
+    CardView myCookBook; //declaring buttons
     Button myCategories;
     AlertDialog dialog;
     ArrayList<Category> createdCategories;
